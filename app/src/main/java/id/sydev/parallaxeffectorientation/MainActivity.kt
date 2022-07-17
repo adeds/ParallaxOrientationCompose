abf3864fee3ed59d82b0ffcb82dac4af6ec23e2c
ade <adeyds@gmail.com>
2022-07-17 21:17:25 +0700

    init project

---
+++ app/src/main/java/id/sydev/parallaxeffectorientation/MainActivity.kt
@@ -0,0 +1,43 @@
+package id.sydev.parallaxeffectorientation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import id.sydev.parallaxeffectorientation.ui.theme.ParallaxEffectOrientationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ParallaxEffectOrientationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ParallaxEffectOrientationTheme {
+        Greeting("Android")
+    }
+}
